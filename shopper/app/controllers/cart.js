8c1bd17a66203f7aba893449b2c0d6fc5a2fe8e3
Shashike J <shashikejayatunge@gmail.com>
2021-08-13 11:05:54 -0400

    use getters to calculate the subtotal in the controller

--- shopper/app/controllers/cart.js
+++ shopper/app/controllers/cart.js
@@ -1,7 +1,18 @@
 import Controller from '@ember/controller';
 
 export default class CartController extends Controller {
-  subtotal = 0
-  tax = 0
-  total = 0
+  get subTotal() {
+    return this.model.reduce((acc, item) => {
+      return acc + item.price
+    }, 0)
+  }
+
+  get tax() {
+    return 0.13 * this.subTotal
+  }
+
+  get total() {
+    return this.subTotal + this.tax
+  }
+
 }
